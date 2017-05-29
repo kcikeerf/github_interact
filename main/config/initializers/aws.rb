5fd92a0980c6a90eb0ee73d761a4d819e6dae7ac
sunsc <sunsc@qidianbj.com>
2017-05-30 00:47:22 +0800

    aws init

---
+++ main/config/initializers/aws.rb
@@ -0,0 +1,9 @@
+# require 'my_aws'
+
+Aws.config.update({
+  credentials: Aws::Credentials.new(
+    'AKIAIS5JBWHSP4XBXHNA',
+    'Kl1Q93vrsJKxhzpYq5PCw9MQfILHwE67FPzl/w2I'
+    ),
+  region: "us-east-2"
+})
