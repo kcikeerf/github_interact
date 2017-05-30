13858cf3e209e1620f484e41fd5f0a0249272935
sunsc <sunsc@qidianbj.com>
2017-05-30 16:05:35 +0800

    update

--- main/app/api/v1/api_v1_auths.rb
+++ main/app/api/v1/api_v1_auths.rb
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+require 'net/http'
 
 module ApiV1Auths
   class API < Grape::API
@@ -26,7 +27,7 @@ module ApiV1Auths
         state_str = SecureRandom.urlsafe_base64(nil, false)
         next_location = "https://github.com/login/oauth/authorize?" +
           "response_type=code&" +
-          "redirect_uri=http://protobuilder.io/api/v1/auths/github_callback&" +
+          "redirect_uri=http://protobuilder.io/api/v1/auths/github_callback://&" +
           "scope=public_repo,read:org,user:email&" +
           "state=" + state_str + "&" +
           "client_id=23ab448fa68cded59495"
@@ -41,7 +42,6 @@ module ApiV1Auths
 
       desc ''
       params do
-        requires :private, type: Boolean, default: false
       end
       get :github_callback do
         github = Github.new client_id: '23ab448fa68cded59495', client_secret: '212ed35e9b844837d7a671606b30d516d1d5bd95'
