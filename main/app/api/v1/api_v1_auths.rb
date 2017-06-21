3e14e0204851109bb55d41a8b99ebde94be448ff
sunsc <sunsc@qidianbj.com>
2017-06-21 22:44:08 +0800

    update

--- main/app/api/v1/api_v1_auths.rb
+++ main/app/api/v1/api_v1_auths.rb
@@ -22,14 +22,17 @@ module ApiV1Auths
       desc ''
       params do
         requires :private, type: Boolean, default: false
+        optional :callback_url, type: String
       end
       get :github_login do
         scope_arr=params[:private] ? %W{ repo } : %W{ public_repo }
         scope_arr << %W{ delete_repo read:org user:email }
         state_str = SecureRandom.urlsafe_base64(nil, false)
+        callback_prefix = "http://protobuilder.io/api/v1/auths/github_callback"
+        callback_url = callback_prefix + "?forward_to=" + params[:callback_url]
         next_location = "https://github.com/login/oauth/authorize?" +
           "response_type=code&" +
-          "redirect_uri=http://protobuilder.io/api/v1/auths/github_callback&" +
+          "redirect_uri=" + callback_url + "&" +
           "scope=" + scope_arr.join(',') + "&" +
           "state=" + state_str + "&" +
           "client_id=" + Common::ClientId
@@ -44,11 +47,13 @@ module ApiV1Auths
       desc ''
       params do
         requires :code, type: String
+        requires :forward_to, type: String
       end
       get :github_callback do
-        token = github.get_token(params[:code])        
+        token = github.get_token(params[:code])
+        forward_url = params[:forward_to] || "http://protobuilder.io"
         status 307
-        header 'location', "http://protobuilder.io/?access_token=#{token.token}"
+        header 'location', forward_url + "?access_token=#{token.token}"
       end
 
       ###########
@@ -59,7 +64,7 @@ module ApiV1Auths
       get :github_revoke do
         token = github.get_token(params[:code])        
         status 307
-        header 'location', "http://protobuilder.io/?access_token=#{token.token}"
+        header 'location', "http://protobuilder.io?access_token=#{token.token}"
       end
 
       ###########      
